5d3e43092241c201c22ddfd66c2e2ade9390583d
Boris Bulovan <bulovan.boris@gmail.com>
2023-11-10 18:48:31 +0100

    Sign-in sa PageObjectMedotom izmene!

--- test/cypress/integration/examples/sign-in.js
+++ test/cypress/integration/examples/sign-in.js
@@ -1,63 +1,58 @@
 //cypress  - Spec
 /// <reference types="Cypress" />
-// import { POM } from '../../fixtures/pom';
+import { SignInPage } from "../../fixtures/sign-in-page";
 
 describe('Sign-in functionality test!', function () {
-    const link = 'https://magento.softwaretestingboard.com/';
-    const signInHome = '.panel > .header > .authorization-link > a';
-    const emailField = '#email';
-    const pwdField = '#pass';
-    const signInBtn = '#send2';
+   const sip = new SignInPage();
+
     const validEmail = 'bulovan.boris@gmail.com';
     const validPassword = 'Pajaparadajz023';
-    const signInCheck = '.greet:visible'
     const invalidEmail = 'invalid@invalid.invalid';
     const invalidPassword = '@Sifra123';
-    const wrongFormEmail = 'boris.bulovan';
+    const wrongFormatEmail = 'boris.bulovan';
 
-    // it('Valid crecdentials used for sign-in', function () {
-    //     // const pom = new POM();
-    //     cy.visit(link);
-    //     cy.get(signInHome).contains('Sign In').click();
-    //     cy.get(emailField).type(validEmail);
-    //     cy.get(pwdField).type(validPassword);
-    //     cy.get(signInBtn).click();
-    //     cy.get(signInCheck).contains('Welcome');
-    // })
+    it('Valid crecdentials used for sign-in', function () {
+        sip.elements.homePageLink();
+        sip.elements.signInHome().contains('Sign In').click();
+        sip.elements.emailField().type(validEmail);
+        sip.elements.pwdField().type(validPassword);
+        sip.elements.signInBtn().click();
+        sip.elements.signInCheck().contains('Welcome');
+    })
 
-    // it('invalid email and invalid password used for sign-in', function () {
-    //     cy.visit(link);
-    //     cy.get(signInHome).contains('Sign In').click();
-    //     cy.get(emailField).type(invalidEmail);
-    //     cy.get(pwdField).type(invalidPassword);
-    //     cy.get(signInBtn).click();
-    //     cy.get('.message-error').contains('The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.');
-    // })
+    it('invalid email and invalid password used for sign-in', function () {
+        sip.elements.homePageLink();
+        sip.elements.signInHome().contains('Sign In').click();
+        sip.elements.emailField().type(invalidEmail);
+        sip.elements.pwdField().type(invalidPassword);
+        sip.elements.signInBtn().click();
+        sip.messages.incorrectSignInMsg().contains('The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.');
+    })
 
-    // it('valid email and invalid password used for sign-in', function () {
-    //     cy.visit(link);
-    //     cy.get(signInHome).contains('Sign In').click();
-    //     cy.get(emailField).type(validEmail);
-    //     cy.get(pwdField).type(invalidPassword);
-    //     cy.get(signInBtn).click();
-    //     cy.get('.message-error').contains('The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.');
-    // })
+    it('valid email and invalid password used for sign-in', function () {
+        sip.elements.homePageLink();
+        sip.elements.signInHome().contains('Sign In').click();
+        sip.elements.emailField().type(validEmail);
+        sip.elements.pwdField().type(invalidPassword);
+        sip.elements.signInBtn().click();
+        sip.messages.incorrectSignInMsg().contains('The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.');
+    })
 
-    // it('wrong format email and valid password used for sign-in ', function () {
-    //     cy.visit(link);
-    //     cy.get(signInHome).contains('Sign In').click();
-    //     cy.wait(2000);
-    //     cy.get(signInBtn).contains('Sign In').click();
-    //     cy.get('#email-error').contains('This is a required field.');
-    //     cy.get('#pass-error').contains('This is a required field.');
-    // })
+    it('wrong format email and valid password used for sign-in ', function () {
+        sip.elements.homePageLink();
+        sip.elements.signInHome().contains('Sign In').click();
+        cy.wait(2000);
+        sip.elements.signInBtn().click();
+        sip.messages.emailErrorMsg().contains('This is a required field.');
+        sip.messages.pwdErrorMsg().contains('This is a required field.');
+    })
 
-    // it('wrong format email and valid password used for sign-in ', function () {
-    //     cy.visit(link);
-    //     cy.get(signInHome).contains('Sign In').click();
-    //     cy.get(emailField).type(wrongFormEmail);
-    //     cy.get(pwdField).type(validPassword);
-    //     cy.get(signInBtn).click();
-    //     cy.get('#email-error').contains('Please enter a valid email address (Ex: johndoe@domain.com).');
-    // })
+    it('wrong format email and valid password used for sign-in ', function () {
+        sip.elements.homePageLink();
+        sip.elements.signInHome().contains('Sign In').click();
+        sip.elements.emailField().type(wrongFormatEmail);
+        sip.elements.pwdField().type(validPassword);
+        sip.elements.signInBtn().click();
+        sip.messages.emailErrorMsg().contains('Please enter a valid email address (Ex: johndoe@domain.com).');
+    })
 })
